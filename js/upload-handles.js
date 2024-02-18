f23a793864ddc9a1a76949593ff03163873791a7
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2024-02-18 01:04:28 -0800

    Update upload-handles.js

--- js/upload-handles.js
+++ js/upload-handles.js
@@ -148,7 +148,7 @@ let uploadToGithub = function ( base64Data, fileName) {
         isImage = true;
     }
     // 固定上传的内容为 hello-world 的 md 文件
-    
+    const name = /images;
     
     // 判断当前是否有任务
     if (viewMap.checkIsHasTask()) return;
@@ -156,7 +156,7 @@ let uploadToGithub = function ( base64Data, fileName) {
     viewMap.setUploading();
     $.ajax({
         type: 'PUT',
-        url: `https://api.github.com/repos/${configObj.userAndRepo}/contents${configObj.path}/${new Date().Format("yyyy")}/${new Date().Format("MM")}/${new Date().Format("dd")}/${fileName}`,
+        url: `https://api.github.com/repos/${configObj.userAndRepo}/contents${name}/${new Date().Format("yyyy")}/${new Date().Format("MM")}/${new Date().Format("dd")}/${fileName}`,
         headers: {
             'Authorization': 'token ' + configObj.token,
             'Content-Type': 'application/json'
