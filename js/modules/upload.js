27b82017197daeded1fd402bb4921cfa0da8b5e3
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2024-02-18 17:39:10 -0800

    Update upload.js

--- js/modules/upload.js
+++ js/modules/upload.js
@@ -23,14 +23,14 @@ import { fileDataProcessing,uploadToGithub } from '../upload-handles.js'
 bindCopy(".resource_box", ".copyUrl", "href", "click");
 // -- 上传前公共文件上传类 -- 
 let UploadFromFile = async function (file) {
- // file = await fileDataProcessing(file);
-  //if (file == null) return;
- // file = await ImgFileCheckCompression(file);
-  //if (file == null) return;
-  //transitionChangeTitle("base64...");
-  //let base64Data = await FileToBase64(file);
-  // 还原标题
-  //transitionChangeTitle(); 
+ file = await fileDataProcessing(file);
+  if (file == null) return;
+ file = await ImgFileCheckCompression(file);
+  if (file == null) return;
+  transitionChangeTitle("base64...");
+  let base64Data = await FileToBase64(file);
+   还原标题
+  transitionChangeTitle(); 
   uploadToGithub(base64Data, file.name);
 
 }
